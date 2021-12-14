216a2b272220ca66bda395ca4358fe48009a0cee
fedyuchuktimothey <95448521+fedyuchuktimothey@users.noreply.github.com>
2021-12-14 06:42:17 +0200

    Crush all functions

--- app/src/main/java/com/example/deka/fragments/students_functions/AllStudentsListFragment.kt
+++ app/src/main/java/com/example/deka/fragments/students_functions/AllStudentsListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.deka.fragments.students_functions
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,13 +24,30 @@ class AllStudentsListFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_groups_list, container, false)
         val db = Firebase.firestore
-        val studentsList = ArrayList<HashMap<String, String>>()
+        val studentslist = ArrayList<HashMap<String, String>>()
+        val student = HashMap<String,String >()
         val simpleAdapter = SimpleAdapter(
-            activity, studentsList,
+            activity, studentslist,
             R.layout.multilines_list, arrayOf("fullname", "group"), intArrayOf(R.id.full_name, R.id.group)
         )
-        val listview=activity?.findViewById(R.id.listView) as ListView
-        listview.adapter=simpleAdapter
+
+        db.collection("Students")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val name = document.getString("name").toString()
+                    val surname= document.getString("surname").toString()
+                    val group = document.getString("group").toString()
+                    Log.d("Name",name)
+                    student.put("fullname", "$surname $name")
+                    student.put("group", group)
+                    studentslist.add(student)
+                    val listview=activity?.findViewById(R.id.listView) as ListView
+                    listview.adapter=simpleAdapter
+                }
+            }
+            .addOnFailureListener { exception ->
+            }
 
         return view
     }
