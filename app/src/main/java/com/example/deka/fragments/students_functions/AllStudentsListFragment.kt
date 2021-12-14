3240f5c8defd9b5b644093edb979a2c56b49dabe
fedyuchuktimothey <95448521+fedyuchuktimothey@users.noreply.github.com>
2021-12-14 14:47:24 +0200

    All students
    Need fix list view first element

--- app/src/main/java/com/example/deka/fragments/students_functions/AllStudentsListFragment.kt
+++ app/src/main/java/com/example/deka/fragments/students_functions/AllStudentsListFragment.kt
@@ -23,9 +23,9 @@ class AllStudentsListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_groups_list, container, false)
+        var listview:ListView
         val db = Firebase.firestore
         val studentslist = ArrayList<HashMap<String, String>>()
-        val student = HashMap<String,String >()
         val simpleAdapter = SimpleAdapter(
             activity, studentslist,
             R.layout.multilines_list, arrayOf("fullname", "group"), intArrayOf(R.id.full_name, R.id.group)
@@ -33,22 +33,21 @@ class AllStudentsListFragment: Fragment() {
 
         db.collection("Students")
             .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    val name = document.getString("name").toString()
-                    val surname= document.getString("surname").toString()
-                    val group = document.getString("group").toString()
-                    Log.d("Name",name)
-                    student.put("fullname", "$surname $name")
-                    student.put("group", group)
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val student = HashMap<String,String >()
+                    student["fullname"] = document.id
+                    student["group"] = document.data["group"].toString()
                     studentslist.add(student)
-                    val listview=activity?.findViewById(R.id.listView) as ListView
+                    listview=activity?.findViewById(R.id.listView) as ListView
                     listview.adapter=simpleAdapter
+                    Log.d("All documents", "${document.id} => ${document.data["group"]}")
+                    Log.d("Student", student.toString())
+
                 }
             }
             .addOnFailureListener { exception ->
             }
-
         return view
     }
 }
