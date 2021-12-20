ed3fb95a4865b7c1c758544f11b23ea4fc8b9008
fedyuchuktimothey <95448521+fedyuchuktimothey@users.noreply.github.com>
2021-12-20 23:43:37 +0200

    Start navigation and Find functions

--- app/src/main/java/com/example/deka/fragments/students_functions/StudFunctionsFragment.kt
+++ app/src/main/java/com/example/deka/fragments/students_functions/StudFunctionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.deka.R
 
 class StudFunctionsFragment:Fragment() {
@@ -15,34 +16,23 @@ class StudFunctionsFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_stud_functions, container, false)
+
         //Add student button
         val addStud = view.findViewById(R.id.addStud) as Button
-        val addStudent = AddStudentFragment()
-        addStud.setOnClickListener() {
-            activity?.supportFragmentManager?.beginTransaction()
-                ?.replace(R.id.container, addStudent)?.addToBackStack("StudFunctionsFragment")?.commit()
-        }
+        addStud.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.addStudentFragment, null))
+
         //Remove student button
         val removeStud = view.findViewById(R.id.removeStud) as Button
-        val removeStudent = RemoveStudentFragment()
-        removeStud.setOnClickListener() {
-            activity?.supportFragmentManager?.beginTransaction()
-                ?.replace(R.id.container, removeStudent)?.addToBackStack("StudFunctionsFragment")?.commit()
-        }
+        removeStud.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.removeStudentFragment, null))
+
         //Edit student button
         val changeStudent = view.findViewById(R.id.changeStud) as Button
-        val changeStudentInfo = ChangeStudentInfoFragment()
-        changeStudent.setOnClickListener() {
-            activity?.supportFragmentManager?.beginTransaction()
-                ?.replace(R.id.container, changeStudentInfo)?.addToBackStack("StudFunctionsFragment")?.commit()
-        }
+        changeStudent.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.changeStudentInfoFragment, null))
+
         // All students button
         val showAllStud = view.findViewById(R.id.showAllStud) as Button
-        val allStudentsList = AllStudentsListFragment()
-        showAllStud.setOnClickListener() {
-            activity?.supportFragmentManager?.beginTransaction()
-                ?.replace(R.id.container, allStudentsList)?.addToBackStack("StudFunctionsFragment")?.commit()
-        }
+        showAllStud.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.allStudentsListFragment, null))
+
         return view
     }
 }
