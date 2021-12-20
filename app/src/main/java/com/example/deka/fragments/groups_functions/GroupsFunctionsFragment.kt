6c98cd74235ead3833c58a6cdbb6234defdf9360
fedyuchuktimothey <95448521+fedyuchuktimothey@users.noreply.github.com>
2021-12-21 01:07:14 +0200

    Add group functions layout

--- app/src/main/java/com/example/deka/fragments/groups_functions/GroupsFunctionsFragment.kt
+++ app/src/main/java/com/example/deka/fragments/groups_functions/GroupsFunctionsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.deka.R
 
 class GroupsFunctionsFragment: Fragment() {
@@ -14,6 +16,9 @@ class GroupsFunctionsFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_groups_functions, container, false)
+        // Remove certain group
+        val findStudentData = view.findViewById(R.id.findStudentData) as Button
+        findStudentData.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.dataStudFormFragment, null))
         return view
     }
 }
